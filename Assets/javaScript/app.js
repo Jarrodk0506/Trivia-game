10b8882a8444cba379103626a410bd55d45ee1f6
Jarrod Krasnoff <Jarrodk0506@outlook.com>
2019-07-10 18:33:24 -0400

    Successfully got questions 1-3 working and collecting correct, incorrect, and unanswered questions

--- Assets/javaScript/app.js
+++ Assets/javaScript/app.js
@@ -1,101 +1,356 @@
 $(document).ready(function () {
 
+console.log("3,2,1,4,2");
+
 var correct = 0;
 var incorrect = 0;
 var unanswered = 0;
 var timer = 5;
-var timerOn = false;
 var timerId;
 
 var questions = {
     question1: [
         question = "This is the first question",
-        answer1 = "This is the first answer",
-        answer2 = "This is the second answer",
-        answer3 = "this is the third answer",
-        answer4 = "this is the fourth answer"
+        answer1 = "1",
+        answer2 = "2",
+        answer3 = "3",
+        answer4 = "4"
     ]
     ,
     question2: [
-        question = "",
-        answer1 = "",
-        answer2 = "",
-        answer3 = "",
-        answer4 = ""
+        question = "This is the Second Question",
+        answer1 = "1",
+        answer2 = "2",
+        answer3 = "3",
+        answer4 = "4"
     ]
     ,
     question3: [
-        question = "",
-        answer1 = "",
-        answer2 = "",
-        answer3 = "",
-        answer4 = ""
+        question = "This is the Third Question",
+        answer1 = "1",
+        answer2 = "2",
+        answer3 = "3",
+        answer4 = "4"
     ]
     ,
     question4: [
-        question = "",
-        answer1 = "",
-        answer2 = "",
-        answer3 = "",
-        answer4 = ""
+        question = "This is the Fourth question",
+        answer1 = "1",
+        answer2 = "2",
+        answer3 = "3",
+        answer4 = "4"
     ]
     ,
     question5: [
-        question = "",
-        answer1 = "",
-        answer2 = "",
-        answer3 = "",
-        answer4 = ""
+        question = "This is the Fifth question",
+        answer1 = "1",
+        answer2 = "2",
+        answer3 = "3",
+        answer4 = "4"
     ]
     
 }
 
-function decrement(){
-    timer--;
-    $("#time").html("Time left: " + timer);
-    console.log(timer);
-    if(timer == 0){
-        stop();
-    }
-}
 
 function stop(){
     clearInterval(timerId);
 }
 
-//start setTimer
-function setTimer(){
-    clearInterval(timerId);
-    timerId =  setInterval(decrement, 1000);
-}; // end setTimer
+
+
+
 
 
 //Start button
 $("#start").click(function () {       
     $("#start").addClass('d-none');
     $("#question").removeClass('d-none');
-    $("#answer1").removeClass('d-none');       //display first question
+    $("#answer1").removeClass('d-none');       //display first question when you hit the start button
     $("#answer2").removeClass('d-none');
     $("#answer3").removeClass('d-none');
     $("#answer4").removeClass('d-none');
-    $("#time").html("Time left: " + timer);
+    $("#time").removeClass('d-none');
+    questionOne();
+});
+
+
+function questionOne(){
+    timer = 4
+    $("#title").html("Question 1");
     $("#question").html(questions.question1[0]);
-    $("#answer1").html(questions.question1[1]);       //display first question
+    $("#answer1").html(questions.question1[1]);       //display First question
     $("#answer2").html(questions.question1[2]);
     $("#answer3").html(questions.question1[3]);
     $("#answer4").html(questions.question1[4]);
-    setTimer();
-});
+    $("#time").html("Time left: " + timer);
 
-// function questionOne(){
-    
-//     $("#time").html("Time left: " + timer);
-//     $("#question").html(questions.question1[0]);
-//     $("#answer1").html(questions.question1[1]);       //display first question
-//     $("#answer2").html(questions.question1[2]);
-//     $("#answer3").html(questions.question1[3]);
-//     $("#answer4").html(questions.question1[4]);
-// };
+    timerId =  setInterval(function(){      //start timer
+        $("#time").html("Time left: " + timer);
+        timer--;
+
+            $('#answer1, #answer2, #answer3, #answer4').click(function () { //start button click
+                // correct
+                if(this.id == "answer3") {
+                stop();
+                timer = 3;
+                $("#title").html("Correct!");
+                $("#question").addClass('d-none');
+                $("#answer1").addClass('d-none');       
+                $("#answer2").addClass('d-none');
+                $("#answer3").addClass('d-none');
+                $("#answer4").addClass('d-none');
+                $("#time").html("Time left: " + timer);
+                    timerId =  setInterval(function(){
+                        $("#time").html("Time left: " + timer);
+                            timer--;
+                            if(timer == -1){
+                                stop();
+                                correct++; 
+                                console.log("correct " + correct);
+                                console.log("incorrect " + incorrect);
+                                console.log("unanswered " + unanswered);
+                                questionTwo();
+                        };
+                    }, 1000);
+                } // end correct
+                // start incorrect
+                else if(this.id !== "answer3"){
+                    stop();
+                    timer = 3;
+                $("#title").html("Incorrect!");
+                $("#time").html("Time left: " + timer);
+                $("#question").addClass('d-none');
+                $("#answer1").addClass('d-none');       
+                $("#answer2").addClass('d-none');
+                $("#answer3").addClass('d-none');
+                $("#answer4").addClass('d-none');
+                    timerId =  setInterval(function(){
+                        $("#time").html("Time left: " + timer);
+                            timer--;
+                            if(timer == -1){
+                                stop();
+                                incorrect++;
+                                console.log("correct " + correct);
+                                console.log("incorrect " + incorrect);
+                                console.log("unanswered " + unanswered);
+                                questionTwo();
+                        };
+                    }, 1000);
+                }//end incorrect
+            }); //end button click
+                if(timer == -1){
+                    stop();
+                    timer = 3;
+                $("#title").html("No answer?");
+                $("#time").html("Time left: " + timer);
+                $("#question").addClass('d-none');
+                $("#answer1").addClass('d-none');
+                $("#answer2").addClass('d-none');        
+                $("#answer3").addClass('d-none');
+                $("#answer4").addClass('d-none');
+                    timerId =  setInterval(function(){
+                        $("#time").html("Time left: " + timer);
+                            timer--;
+                            if(timer == -1){
+                                stop();
+                                unanswered++;
+                                console.log("correct " + correct);
+                                console.log("incorrect " + incorrect);
+                                console.log("unanswered " + unanswered);
+                                questionTwo();
+                        };
+                    }, 1000);
+                }
+    },1000);//end timerId
+}  //end questionOne
+
+//question2
+function questionTwo(){
+    timer = 4
+    $("#question").removeClass('d-none');
+    $("#answer1").removeClass('d-none');       
+    $("#answer2").removeClass('d-none');
+    $("#answer3").removeClass('d-none');
+    $("#answer4").removeClass('d-none');
+    $("#title").html("Question 2");
+    $("#question").html(questions.question2[0]);
+    $("#answer1").html(questions.question2[1]);       //display First question
+    $("#answer2").html(questions.question2[2]);
+    $("#answer3").html(questions.question2[3]);
+    $("#answer4").html(questions.question2[4]);
+    $("#time").html("Time left: " + timer);
+
+    timerId =  setInterval(function(){      //start timer
+        $("#time").html("Time left: " + timer);
+        timer--;
+
+            $('#answer1, #answer2, #answer3, #answer4').click(function () { //start button click
+                // correct
+                if(this.id == "answer2") {
+                stop();
+                timer = 3;
+                $("#title").html("Correct!");
+                $("#question").addClass('d-none');
+                $("#answer1").addClass('d-none');
+                $("#answer2").addClass('d-none');     
+                $("#answer3").addClass('d-none');
+                $("#answer4").addClass('d-none');
+                $("#time").html("Time left: " + timer);
+                    timerId =  setInterval(function(){
+                        $("#time").html("Time left: " + timer);
+                            timer--;
+                            if(timer == -1){
+                                stop();
+                                correct++; 
+                                console.log("correct " + correct);
+                                console.log("incorrect " + incorrect);
+                                console.log("unanswered " + unanswered);
+                                questionThree();
+                        };
+                    }, 1000);
+                } // end correct
+                // start incorrect
+                else if(this.id !== "answer2"){
+                    stop();
+                    timer = 3;
+                $("#title").html("Incorrect!");
+                $("#time").html("Time left: " + timer);
+                $("#question").addClass('d-none');
+                $("#answer1").addClass('d-none');
+                $("#answer2").addClass('d-none');       
+                $("#answer3").addClass('d-none');
+                $("#answer4").addClass('d-none');
+                    timerId =  setInterval(function(){
+                        $("#time").html("Time left: " + timer);
+                            timer--;
+                            if(timer == -1){
+                                stop();
+                                incorrect++;
+                                console.log("correct " + correct);
+                                console.log("incorrect " + incorrect);
+                                console.log("unanswered " + unanswered);
+                                questionThree();
+                        };
+                    }, 1000);
+                }//end incorrect
+            }); //end button click
+                if(timer == -1){
+                    stop();
+                    timer = 3;
+                $("#title").html("No answer?");
+                $("#time").html("Time left: " + timer);
+                $("#question").addClass('d-none');
+                $("#answer1").addClass('d-none');  
+                $("#answer2").addClass('d-none');    
+                $("#answer3").addClass('d-none');
+                $("#answer4").addClass('d-none');
+                    timerId =  setInterval(function(){
+                        $("#time").html("Time left: " + timer);
+                            timer--;
+                            if(timer == -1){
+                                stop();
+                                unanswered++;
+                                console.log("correct " + correct);
+                                console.log("incorrect " + incorrect);
+                                console.log("unanswered " + unanswered);
+                                questionThree();
+                        };
+                    }, 1000);
+                }
+    },1000);//end timerId
+}  //end questionTwo
+
+function questionThree(){
+    timer = 4
+    $("#question").removeClass('d-none');
+    $("#answer1").removeClass('d-none');       
+    $("#answer2").removeClass('d-none');
+    $("#answer3").removeClass('d-none');
+    $("#answer4").removeClass('d-none');
+    $("#title").html("Question 3");
+    $("#question").html(questions.question3[0]);
+    $("#answer1").html(questions.question3[1]);       //display First question
+    $("#answer2").html(questions.question3[2]);
+    $("#answer3").html(questions.question3[3]);
+    $("#answer4").html(questions.question3[4]);
+    $("#time").html("Time left: " + timer);
+
+    timerId =  setInterval(function(){      //start timer
+        $("#time").html("Time left: " + timer);
+        timer--;
+
+            $('#answer1, #answer2, #answer3, #answer4').click(function () { //start button click
+                // correct
+                if(this.id == "answer1") {
+                stop();
+                timer = 3;
+                $("#title").html("Correct!");
+                $("#question").addClass('d-none');
+                $("#answer1").addClass('d-none'); 
+                $("#answer2").addClass('d-none');       
+                $("#answer3").addClass('d-none');
+                $("#answer4").addClass('d-none');
+                $("#time").html("Time left: " + timer);
+                    timerId =  setInterval(function(){
+                        $("#time").html("Time left: " + timer);
+                            timer--;
+                            if(timer == -1){
+                                stop();
+                                correct++; 
+                                console.log("correct " + correct);
+                                console.log("incorrect " + incorrect);
+                                console.log("unanswered " + unanswered);
+                        };
+                    }, 1000);
+                } // end correct
+                // start incorrect
+                else if(this.id !== "answer2"){
+                    stop();
+                    timer = 3;
+                $("#title").html("Incorrect!");
+                $("#time").html("Time left: " + timer);
+                $("#question").addClass('d-none');
+                $("#answer1").addClass('d-none'); 
+                $("#answer2").addClass('d-none');       
+                $("#answer3").addClass('d-none');
+                $("#answer4").addClass('d-none');
+                    timerId =  setInterval(function(){
+                        $("#time").html("Time left: " + timer);
+                            timer--;
+                            if(timer == -1){
+                                stop();
+                                incorrect++;
+                                console.log("correct " + correct);
+                                console.log("incorrect " + incorrect);
+                                console.log("unanswered " + unanswered);
+                        };
+                    }, 1000);
+                }//end incorrect
+            }); //end button click
+                if(timer == -1){
+                    stop();
+                    timer = 3;
+                $("#title").html("No answer?");
+                $("#time").html("Time left: " + timer);
+                $("#question").addClass('d-none');
+                $("#answer1").addClass('d-none');
+                $("#answer2").addClass('d-none');       
+                $("#answer3").addClass('d-none');
+                $("#answer4").addClass('d-none');
+                    timerId =  setInterval(function(){
+                        $("#time").html("Time left: " + timer);
+                            timer--;
+                            if(timer == -1){
+                                stop();
+                                unanswered++;
+                                console.log("correct " + correct);
+                                console.log("incorrect " + incorrect);
+                                console.log("unanswered " + unanswered);
+                        };
+                    }, 1000);
+                }
+    },1000);//end timerId
+}  //end questionThree
 
 
 
@@ -106,4 +361,6 @@ $("#start").click(function () {
 
 
 
-});
+
+
+}); // end
