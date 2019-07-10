762c44de1c21efc3e02ec56e93bf9c9ee8fe43a8
Jarrod Krasnoff <Jarrodk0506@outlook.com>
2019-07-09 21:10:20 -0400

    Displays timer, first question and answers

--- Assets/javaScript/app.js
+++ Assets/javaScript/app.js
@@ -8,11 +8,11 @@ var interval;
 
 var questions = {
     question1: [
-        question = "",
-        answer1 = "",
-        answer2 = "",
-        answer3 = "",
-        answer4 = ""
+        question = "This is the first question",
+        answer1 = "This is the first answer",
+        answer2 = "This is the second answer",
+        answer3 = "this is the third answer",
+        answer4 = "this is the fourth answer"
     ]
     ,
     question2: [
@@ -62,9 +62,23 @@ $("#time").click(function () {
     $("#answer3").removeClass('d-none');
     $("#answer4").removeClass('d-none');
     $("#time").html("Time left: " + timer);
+    $("#question").html(questions.question1[0]);
+    $("#answer1").html(questions.question1[1]);       //display first question
+    $("#answer2").html(questions.question1[2]);
+    $("#answer3").html(questions.question1[3]);
+    $("#answer4").html(questions.question1[4]);
+    
 });
 
-
+function questionOne(){
+    
+    $("#time").html("Time left: " + timer);
+    $("#question").html(questions.question1[0]);
+    $("#answer1").html(questions.question1[1]);       //display first question
+    $("#answer2").html(questions.question1[2]);
+    $("#answer3").html(questions.question1[3]);
+    $("#answer4").html(questions.question1[4]);
+};
 
 
 
